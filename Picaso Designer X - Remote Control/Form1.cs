2fc6ba4351e66e2008ac4bad932c7cd204a3acc8
Пользователь <@DESCTOP-LIFANDI>
2023-04-24 01:38:52 +0300

    Добавьте файлы проекта.

---
+++ Picaso Designer X - Remote Control/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picaso_Designer_X___Remote_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
